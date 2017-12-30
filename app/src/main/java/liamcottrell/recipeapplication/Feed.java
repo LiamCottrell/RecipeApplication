216e5b6ae07a811b587d0251e5b6d1682f3ad3a6
Liam Cottrell <liamcottrell@hotmail.co.uk>
2017-12-30 20:17:42 +0000

    Changed feed.java to reflect changed id names of nav items.
    Signed-off-by: Liam Cottrell <liamcottrell@hotmail.co.uk>

--- app/src/main/java/liamcottrell/recipeapplication/Feed.java
+++ app/src/main/java/liamcottrell/recipeapplication/Feed.java
@@ -50,17 +50,15 @@ public class Feed extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_account) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_recipe) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_create_recipe) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_preferences) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_git) {
 
         }
 
